1ea66e6842e3fa72539c50d8e8f8ecfd36c4a594
Krishnan S <skrishnan2001@gmail.com>
2022-01-25 12:25:09 +0530

    feat: Pushing the project files

---
+++ StockPricePredictor.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+
+def isLeapYear(y):
+    if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+        return True
+    return False
+
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+year = int(TODAY[: 4])
+
+st.title('Stock Forecast App')
+
+stocks = ('FB', 'AAPL', 'AMZN', 'NFLX', 'GOOG', 'MSFT', 'GME', 'C', 'GS', 'LTOD.IL', 'INFY', 'MS')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+selection = yf.Ticker(selected_stock)
+company_name = selection.info['longName']
+st.subheader(company_name + "'s Stocks")
+
+period = 0
+n_years = st.slider('Years of prediction:', 1, 4)
+
+for i in range(0, n_years):
+    if (isLeapYear(year + i)):
+        period += 366
+    else:
+        period += 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+
+# Plotting the raw data
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+
+plot_raw_data()
+
+# Predicting the forecast with Prophet.
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Showing and plotting the forecast
+st.subheader('Forecast data')
+st.write(forecast)
+
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
