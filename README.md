a0896e73cf06f782c5c6c4b27a09e32efa52f05b
Krishnan S <skrishnan2001@gmail.com>
2022-03-29 18:54:17 +0530

    fix: Handled type errors and connection errors

---
+++ README.md
@@ -0,0 +1,9 @@
+# Stock-Price-Predictor
+ML based web-app that can predict the price of the stocks of a company for the next 4 years
+
+## Project Overview
+Investment firms, hedge funds and even individuals have been using financial models to better understand market behavior and make profitable investments and trades. A wealth of information is available in the form of historical stock prices and company performance data, suitable for machine learning algorithms to process.
+
+Can we actually predict stock prices with machine learning? Investors make educated guesses by analyzing data. They'll read the news, study the company history, industry trends and other lots of data points that go into making a prediction. The prevailing theories is that stock prices are totally random and unpredictable but that raises the question why top investment banks and financial services hire quantitative analysts to build predictive models. We have this idea of a trading floor being filled with adrenaline infuse men with loose ties running around yelling something into a phone but these days they're more likely to see rows of machine learning experts quietly sitting in front of computer screens. In fact about 70% of all orders on Wall Street are now placed by software, we're now living in the age of the algorithm.
+
+This web-app attempts to predict the stock prices of all the companies listed in `yahoo finance` for the next 4 years based on the data from Jan 2015 to the present date. I'm fetching the past stock information about a company using `Yfinance`, a python package that enables us to fetch historical market data from Yahoo Finance API in a Pythonic way. From this data, using Facebook's `fbprohet library`, I'm predicting the future stock prices and also displaying it graphically for the next 4 years.
--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -7,103 +7,108 @@ import pandas as pd
 from plotly import graph_objs as go
 
 
-def listOfStockSymbols():
-    yahooTickers = pd.read_csv('dataset/Yahoo Ticker Symbols.csv')
-    symbols = yahooTickers['Ticker'].tolist()
-    return symbols
-
 def isLeapYear(y):
-    if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
-        return True
-    return False
+    return (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0)
+
 
 def sideBarHelper(text):
-    st.sidebar.markdown(text)
+    st.sidebar.text(text)
+
 
 def populateSideBar():
     st.sidebar.image(selection.info["logo_url"])
-    st.sidebar.subheader(selection.info['shortName'])
+    st.sidebar.header(selection.info['shortName'])
     sideBarHelper("Sector: " + selection.info['sector'])
     sideBarHelper("Financial Currency: " + selection.info['financialCurrency'])
     sideBarHelper("Exchange: " + selection.info['exchange'])
-
-    st.sidebar.json({
-        "Exchange Time Zone" : selection.info['exchangeTimezoneName'],
-        "Current Price" : selection.info['currentPrice'],
-        "Previous Close" : selection.info['previousClose'],
-        "Day Low" : selection.info['dayLow'],
-        "Day High" : selection.info['dayHigh']
-    })
+    sideBarHelper("Timezone: " + selection.info['exchangeTimezoneName'])
 
 
+def stockPricesToday():
+    today_data = {'Current Price': [selection.info['currentPrice']],
+                  'Previous Close': [selection.info['previousClose']],
+                  'Open': [selection.info['open']],
+                  'Day Low': [selection.info['dayLow']],
+                  'Day High': [selection.info['dayHigh']]
+                  }
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-year = int(TODAY[: 4])
+    df = pd.DataFrame(today_data)
+    # priceChangeToday = selection.info['currentPrice'] - data['Close'][len(data) - 1]  # Current Price - Previous Closing
+    col1, col2 = st.columns(2)
+    priceChangeToday = selection.info['currentPrice'] - selection.info['open']  # Current Price - Previous Closing
+    col1.metric(label="Current Price, Change w.r.t Opening Price", value='%.2f' % selection.info['currentPrice'],
+                delta='%.2f' % priceChangeToday)
 
-st.title('Stock Forecast App')
+    priceChangeYesterday = data['Close'][len(data) - 1] - data['Close'][len(data) - 2] if len(data) >= 2 else 0
+    col2.metric(label="Previous Closing, Previous Day Change", value='%.2f' % data['Close'][len(data) - 1],
+                delta='%.2f' % priceChangeYesterday)
 
-# stocks = listOfStockSymbols()
-stock = st.sidebar.text_input("Symbol", value='AAPL', max_chars=8)
-# selected_stock = st.selectbox('Select dataset for prediction', stocks)
+    st.dataframe(df)
 
-stockSymbols = listOfStockSymbols()
 
-if stock not in stockSymbols:
-    st.error('This company is not listed !')
+@st.cache
+def load_data(ticker):
+    historicData = yf.download(ticker, START, TODAY)
+    historicData.reset_index(inplace=True)
+    return historicData
 
-else:
-    selected_stock = stock
 
-    selection = yf.Ticker(selected_stock)
-    company_name = selection.info['longName']
-    st.subheader(company_name + "'s Stocks")
+def plot_raw_data():
+    # Plotting the raw data
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
 
-    populateSideBar()
+    fig = go.Figure()
+    lastFiveDays = data.tail(10)
+    fig.add_trace(go.Candlestick(x=lastFiveDays['Date'], open=lastFiveDays['Open'], high=lastFiveDays['High'],
+                                 low=lastFiveDays['Low'],
+                                 close=lastFiveDays['Close']))
+    fig.layout.update(title_text='Candle Stick Chart - Last 10 Days Trend', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
 
+
+def pastTrends():
+    st.info(selection.info['longBusinessSummary'])
+    st.subheader('Today')
+    stockPricesToday()
+
+    st.subheader('Last 5 Days Trend')
+    st.write(data.tail())
+
+
+def predictingTheStockPrices():
     period = 0
     n_years = st.slider('Years of prediction:', 1, 4)
 
     for i in range(0, n_years):
-        if (isLeapYear(year + i)):
+        if isLeapYear(year + i):
             period += 366
         else:
             period += 365
 
-
-    @st.cache
-    def load_data(ticker):
-        data = yf.download(ticker, START, TODAY)
-        data.reset_index(inplace=True)
-        return data
-
-
-    data_load_state = st.text('Loading data...')
-    data = load_data(selected_stock)
-    data_load_state.text('Loading data... done!')
-
-    st.subheader('Raw data')
-    st.write(data.tail())
-
-
-    # Plotting the raw data
-    def plot_raw_data():
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-        fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-        st.plotly_chart(fig)
-
-
-    plot_raw_data()
-
-    # Predicting the forecast with Prophet.
     df_train = data[['Date', 'Close']]
     df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-    m = Prophet()
+    model_param = {
+        "daily_seasonality": False,
+        "weekly_seasonality": False,
+        "yearly_seasonality": True,
+        "seasonality_mode": "multiplicative",
+        "growth": "logistic"
+    }
+
+    m = Prophet(**model_param)
+
+    m = m.add_seasonality(name="monthly", period=30, fourier_order=10)
+    m = m.add_seasonality(name="quarterly", period=92.25, fourier_order=10)
+
+    df_train['cap'] = df_train["y"].max() + df_train["y"].std() * 0.05
     m.fit(df_train)
     future = m.make_future_dataframe(periods=period)
+    future['cap'] = df_train['cap'].max()
     forecast = m.predict(future)
 
     # Showing and plotting the forecast
@@ -114,6 +119,51 @@ else:
     fig1 = plot_plotly(m, forecast)
     st.plotly_chart(fig1)
 
-    st.write("Forecast components")
+    st.write("Forecast components - Yearly, Monthly and Quarterly Trends")
     fig2 = m.plot_components(forecast)
     st.write(fig2)
+
+
+# Driver
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+year = int(TODAY[: 4])
+
+st.title('STOCK FORECAST APP')
+
+try:
+    option = st.sidebar.selectbox("Which Dashboard?", ('Past Trends', 'Predict Stock Price'), 0)
+    stock = st.sidebar.text_input("Symbol", value='GOOG')
+    # selected_stock = st.selectbox('Select dataset for prediction', stocks)
+    # stocks = listOfStockSymbols()
+    selected_stock = stock
+
+    data = load_data(selected_stock)
+
+    selection = yf.Ticker(selected_stock)
+    company_name = selection.info['longName']
+    st.subheader(company_name + "'s Stocks")
+
+    populateSideBar()
+
+    if option == 'Past Trends':
+        pastTrends()
+        plot_raw_data()
+
+    if option == 'Predict Stock Price':
+        # Predicting the forecast with Prophet.
+        predictingTheStockPrices()
+except KeyError:
+    st.error('This company is not listed !')
+
+except FileNotFoundError:
+    st.error('No data is available about this stock !')
+
+except TypeError:
+    st.error('No data is available about this stock !')
+
+except ValueError:
+    st.error('Symbol cannot be empty !')
+
+except ConnectionError:
+    st.error('Could not connect to the internet :(')
